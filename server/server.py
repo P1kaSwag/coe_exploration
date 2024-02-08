d45ce3f60abfe60e5f8690c6566877cfc57fd58a
TannerParks <59822998+TannerParks@users.noreply.github.com>
2024-02-08 02:27:58 -0800

    Setup the database and the server connection
    The server and database are now connected using PyMySQL and SQLAlchemy. I made a placeholder function to prove it's working so by running "docker compose up --build" you should be able to navigate to localhost:8000/courses and see the sample output from the test data I put in the database

--- server/server.py
+++ server/server.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import datetime
  
 x = datetime.datetime.now()
  
 # Initializing flask app
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:bowleg.historic.TORI@database:3306/exploration'
  
+ # Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# All this course stuff was just to test out the database connection to the server
+# so anybody can change/repurpose it if they want
+class Course(db.Model):
+    __tablename__ = 'Courses'
+    CRN = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(255), nullable=False)
+    course_description = db.Column(db.Text, nullable=False)
+    credits = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'CRN': self.CRN,
+            'course_name': self.course_name,
+            'course_description': self.course_description,
+            'credits': self.credits
+        }
+
+# Create a route to query Courses and return the data (navigate to localhost:8000/courses)
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    courses = Course.query.all()
+    return jsonify([course.to_dict() for course in courses])
  
 # Route for seeing a data
 @app.route('/api/data')
@@ -20,5 +48,5 @@ def get_time():
  
      
 # Running app
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
