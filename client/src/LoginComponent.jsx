9a2b8328ce4765f5fb882924818c53fdd0352b0d
Morgan <morgancurtis180@gmail.com>
2024-02-13 16:46:27 -0800

    Restructed app and removed redundant files. Added Root component and Error handling
    added dependency npm install react-router-dom for controlling the routes. Also added a Root component which serves as a Navbar and Errorhandling.
    Removed quiz/main.jsx and App/jsx.

--- client/src/LoginComponent.jsx
+++ client/src/LoginComponent.jsx
@@ -3,7 +3,7 @@ const LoginComponent = ( ) => {
     return (
       <div className='login'>
         <h1>Login Page</h1>
-        <div class="login_container">
+        <div className="login_container">
           <label id="userlabel"> Username </label>
           <input type="text" placeholder="Enter Username" name="username"/>
           <label id='passlabel'> Password </label>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { Root, ErrorPage } from './Root'; 
+import LoginComponent from './LoginComponent';
+import Quiz from './quiz/App'; 
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Root><ErrorPage /></Root>,
+    children: [
+      { index: true, element: <LoginComponent /> }, // HomeComponent
+      { 
+        path: "quiz",
+        element: <Quiz />,
+        // Add children routes if needed
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} /> {/* Using RouterProvider */}
+  </React.StrictMode>
+);
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
