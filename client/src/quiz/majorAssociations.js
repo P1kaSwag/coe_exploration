3e2642d4af9a120e690bd728b7739740b4a18ef3
Morgan <morgancurtis180@gmail.com>
2024-02-19 10:18:42 -0800

    added a majorAssociations file
    This file will be used by resultsComponent to determine which majors to award a point to if that major is associated with that answer.

---
+++ client/src/quiz/majorAssociations.js
@@ -0,0 +1,33 @@
+const majorChoiceAssociations = {
+    1: {
+      A: ["Aerospace Engineering"],
+      B: ["Computer Science"],
+      C: ["Electrical Engineering"],
+      D: ["Mechanical Engineering", "Civil Engineering"]
+    },
+    2: {
+      A: ["Computer Science"],
+      B: ["Civil Engineering"],
+      C: ["Aerospace Engineering"],
+      D: ["Mechanical Engineering", "Industrial Engineering"]
+    },
+    3: {
+      A: ["Computer Science", "Electrical Engineering"],
+      B: ["Civil Engineering"],
+      C: ["Aerospace Engineering"],
+      D: ["Mechanical Engineering", "Industrial Engineering"]
+    },
+    4: {
+      A: ["Computer Science"],
+      B: ["Civil Engineering"],
+      C: ["Aerospace Engineering"],
+      D: ["Mechanical Engineering", "Industrial Engineering"]
+    },
+    5: {
+      A: ["Computer Science"],
+      B: ["Civil Engineering"],
+      C: ["Aerospace Engineering"],
+      D: ["Mechanical Engineering", "Industrial Engineering"]
+    }
+  };
+  
