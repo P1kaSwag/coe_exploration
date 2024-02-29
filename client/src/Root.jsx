32359ede65dbc962b7ac645398127126fd6fed7d
Bryan <nguyenb6@oregonstate.edu>
2024-02-28 16:21:30 -0800

    Added profile image to NavBar

--- client/src/Root.jsx
+++ client/src/Root.jsx
@@ -35,6 +35,7 @@ export function Root(props) {
           </li>
           {user && ( // Conditionally render the button if user is logged in
             <li>
+              <img src="poggers.png" alt="Profile Image" class="profile-img"></img>
               <NavLink to="/profile">Profile</NavLink>
             </li>
           )}
