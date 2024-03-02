a8ec9a7171b7a93879c0499b0d7730318e293384
TannerParks <59822998+TannerParks@users.noreply.github.com>
2024-03-02 02:48:49 -0800

    Set up JSON web tokens
    Implemented a quick authentication token that users get when they log in. We don't use them yet but they're set up to be used.
    
    JWTs allow us to securely handle authentication and authorization between the frontend and backend. They also ensure that users can only access and modify their own pets.

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
 import bcrypt
 import datetime
  
@@ -9,15 +11,19 @@ x = datetime.datetime.now()
 # Initializing flask app
 app = Flask(__name__)
 CORS(app)
+jwt = JWTManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:bowleg.historic.TORI@database:3306/exploration'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning and to save resources
+app.config['SECRET_KEY'] = 'super_secret_key'  # Just for testing purposes, should be changed to a more secure key in production
  
  # Create the SQLAlchemy db instance
 db = SQLAlchemy(app)
 
 
-#TODO: Remove/Change the course stuff later
+
+
+# TODO: Remove/Change the course stuff later
 # All this course stuff was just to test out the database connection to the server
 # so anybody can change/repurpose it if they want
 class Course(db.Model):
@@ -112,7 +118,7 @@ def register():
     data = request.json
     username = data.get('username')
     email = data.get('email')
-    password = data.get('password') #FIXME: Hash the password then use that hash to compare to the hash in the database
+    password = data.get('password')
 
     if not username or not email or not password:
         return jsonify({'message': 'All fields are required'})
@@ -135,13 +141,15 @@ def login():
     password = data.get('password')
 
     if not username or not password:
-        return jsonify({'message': 'Username and password are required'})
+        return jsonify({'message': 'Username and password are required'})   # TODO: Change this to or add a 400 status code
 
     user = Users.query.filter_by(username=username).first()
     if user and user.check_password(password):
-        return jsonify({'message': 'Login successful'})
+        # Create a new access token for the user
+        access_token = create_access_token(identity=user.userID)
+        return jsonify({'message': 'Login successful', 'access_token': access_token, 'user': user.to_dict()})   # TODO: Change this to or add a 200 status code
 
-    return jsonify({'message': 'Invalid username or password'})
+    return jsonify({'message': 'Invalid username or password'}) # TODO: Change this to or add a 401 status code
 
 @app.route('/api/majors', methods=['GET'])
 def get_majors():
