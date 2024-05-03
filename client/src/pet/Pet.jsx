0805d1cfe53a02fd5443fe1855fcf8d54c10769c
TannerParks <59822998+TannerParks@users.noreply.github.com>
2024-05-03 14:54:20 -0700

    Outfit switches working

--- client/src/pet/Pet.jsx
+++ client/src/pet/Pet.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useAuth } from '../authentication/AuthComponent';
 import PetMenu from './PetMenu';
 import './pet_styles.css';
-//import walkingAnimation from '../assets/default/walking.png';
-//import idleAnimation from '../assets/default/idle.png';
+import outfitMappings from './outfitConfig';
 
 const Pet = () => {
     const { accessToken } = useAuth();  // Get the access token from the AuthProvider to make authenticated requests
@@ -16,14 +15,18 @@ const Pet = () => {
     const [outfit, setOutfit] = useState('default');
     const [dirtOverlay, setDirtOverlay] = useState('none'); // ['none', 'light', 'heavy']
     const [loadedImages, setLoadedImages] = useState([]);
+    const walkTimeout = useRef(null);
 
     const points = [
-        { left: 40, top: 20, scale: 1 },
         { left: 70, top: 50, scale: 1 },
-        { left: 70, top: 40, scale: 1 },
-        { left: 76, top: 30, scale: 1 },
-        { left: 0, top: 30, scale: 1 },
-        { left: 30, top: 40, scale: 1 },
+        { left: 35, top: 50, scale: 1 },
+        { left: 5, top: 50, scale: 1 },
+        { left: 70, top: 40, scale: 0.9 },
+        { left: 30, top: 40, scale: 0.9 },
+        { left: 76, top: 30, scale: 0.8 },
+        { left: 0, top: 30, scale: 0.8 },
+        { left: 40, top: 20, scale: 0.7 },
+        { left: 65, top: 20, scale: 0.7 },
     ];
 
     const pickRandomPoint = () => {
@@ -77,12 +80,23 @@ const Pet = () => {
     // Preload the pet's images to prevent flickering when transitioning between animations
     const preloadImages = () => {
         const images = [];
-        const states = ['walking', 'eating', 'idle', 'washing', 'petting'];             // Default states for the pet, need to add states for outfits
+        const states = ['walking', 'eating', 'idle', 'washing', 'petting'];
         states.forEach(state => {
             const img = new Image();
-            img.src = `src/assets/${outfit}/${state}.png`;                              // Change path to just default then load outfits as overlays
+            img.src = `src/assets/default/${state}.png`;
             images.push(img);
         });
+
+        // Preload outfit overlays based on the mappings in outfitConfig.jsx
+        if (outfit && outfit !== 'default'){
+            states.forEach(state => {
+                if (outfitMappings[outfit][state]) {
+                    const img = new Image();
+                    img.src = `src/assets/${outfit}/${outfitMappings[outfit][state]}.png`;
+                    images.push(img);
+                }
+            });
+        }
 
         // Preload the dirt overlay images if applicable
         if (dirtOverlay !== 'none') {
@@ -125,12 +139,15 @@ const Pet = () => {
                 // Stop walking animation when at target destination
                 if (reachedTarget) {
                     setAnimationState('idle'); // Set the pet to idle
-                    setTimeout(() => {
-                        const newTarget = pickRandomPoint();
-                        setTargetPosition(newTarget); // Set a new destination
-                        setAnimationState('walking'); // Set the pet to walking
+                    walkTimeout.current = setTimeout(() => {
+                        if (!showMenu){
+                            const newTarget = pickRandomPoint();
+                            setTargetPosition(newTarget); // Set a new destination
+                            console.log('New target:', newTarget);
+                            setAnimationState('walking'); // Start walking again
+                        }
                     }, 3000); // Wait time in milliseconds
-
+                    
                     return prevPosition; // Return current position to prevent state update
                 }
 
@@ -195,9 +212,14 @@ const Pet = () => {
         return () => {
             if (intervalId) {
                 clearInterval(intervalId);
-            }
+            }  
+            //if (walkTimeout) {
+            //    clearTimeout(walkTimeout);
+            clearTimeout(walkTimeout.current);
         };
     }, [targetPosition, animationState, showMenu]);
+
+
 
     // TESTING INTERACTIONS 1 ############################################################################################
     useEffect(() => {
@@ -215,12 +237,15 @@ const Pet = () => {
         setAnimationState('idle'); // Stop the pet from walking
         setShowMenu(true); // Show the menu
         setMenuPosition({ x: event.pageX, y: event.pageY }); // Position the menu at the click location
+        if (walkTimeout) {
+            clearTimeout(walkTimeout.current);
+            walkTimeout.current = null;
+        }
     };
 
     const handleOptionSelected = async (interactionType) => {
         console.log(`Selected option: ${interactionType}`);
         setShowMenu(false);
-        setAnimationState('walking');
 
 
         // TESTING INTERACTIONS 2 ########################################################################################
@@ -231,7 +256,6 @@ const Pet = () => {
         } else if (interactionType === 'pet') {
             setAnimationState('petting');
         }
-
 
 
         if (!accessToken) {
@@ -258,19 +282,30 @@ const Pet = () => {
             console.error(`Error status: ${response.status}`);
         }
 
+        setAnimationState('walking');
+
     };
 
+    const getOutfitImage = (state) => {
+        if (outfit && outfit !== 'default' && outfitMappings[outfit][state]) {
+          return `src/assets/${outfit}/${outfitMappings[outfit][state]}.png`;
+        } else {
+          return '';
+        }
+      };
+
     const debugAnimation = () => {
+        if (outfit === 'default') {
+            setOutfit('ChemicalEngineering');
+        } else {
+            setOutfit('default');
+        }
+    }
+
+    const debugAnimation2 = () => {
         if (animationState === 'walking') {
             setAnimationState('idle');
-        } else if (animationState === 'idle') {
-            setAnimationState('eating');
-        } else if (animationState === 'eating') {
-            setAnimationState('washing');
-        } else if (animationState === 'washing') {
-            setAnimationState('petting');
-        }
-        else {
+        } else {
             setAnimationState('walking');
         }
     }
@@ -285,37 +320,58 @@ const Pet = () => {
                 position: 'absolute',
                 left: '11.1%',
                 top: '30%',
-                }}>Walk</button>
+                }}>{outfit}</button>
+            <button onClick={debugAnimation2} style={{
+                position: 'absolute',
+                left: '11.1%',
+                top: '32%',
+                }}>Idle</button>
+            <button style={{
+                position: 'absolute',
+                left: '0%',
+                top: '90%'}}>test</button>
             {/* DEBUG */}
 
-            <div className='petContainer'> </div>
             
                 <div
-                    key={`${outfit}-${animationState}`}
+                    key={`pet ${animationState}`}
                     className={`pet ${animationState}`}
-                    onClick={handlePetClick}
+                    //onClick={handlePetClick}
                     style={{
-                        backgroundImage: `url('src/assets/${outfit}/${animationState}.png')`,
+                        backgroundImage: `url('src/assets/default/${animationState}.png')`,
                         position: 'absolute',
                         left: `${position.left}%`,
                         top: `${position.top}%`,
                         transform: `scale(${position.scale}) scaleX(${position.flip})`,
-                    }}
-                >
-                    <div className={`dirty ${animationState}`} style={{
-                        backgroundImage: `url('src/assets/dirt/${dirtOverlay}_${animationState}.png')`,
-                        position: 'absolute',
-                        left: '0%',
-                        top: '0%',
-                        width: '100%',
-                        height: '100%',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
                     }}>
-                        
-                    </div>
+                    <div
+                        key={`${outfit} ${animationState}`}
+                        className={`${outfit}-${animationState}`}
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            backgroundImage: `url(${getOutfitImage(animationState)})`,
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                        }}
+                    ></div>
+                    <div className='clickableArea'
+                    onClick={handlePetClick}
+                    style={{
+                        position: 'absolute',
+                        top: '40%',
+                        left: '30%',
+                        width: '40%',
+                        height: '30%',
+                        cursor: 'pointer',
+                    }}
+                    ></div>
+
                 </div>
+
             <div className="statBoard">
                 <h3>{petStats.pet_name} is feeling...</h3>
                 <p>Mood: {petStats.mood}</p>
---
+++ client/src/pet/outfitConfig.jsx
@@ -0,0 +1,25 @@
+const outfitMappings = {
+    default: {
+      walking: 'walking',
+      eating: 'eating',
+      idle: 'idle',
+      petting: 'petting',
+      washing: 'washing'
+    },
+    ChemicalEngineering: {
+      walking: 'idle', // Use idle overlay for walking
+      eating: 'eating',
+      idle: 'idle',
+      petting: 'petting'
+    },
+    RadiationHealthPhysics: {
+      walking: 'walking',
+      eating: 'eating',
+      idle: 'idle',
+      petting: 'idle' // Use idle overlay for petting
+    },
+    // Add other outfits here
+  };
+  
+  export default outfitMappings;
+  
